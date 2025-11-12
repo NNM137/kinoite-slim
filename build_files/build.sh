e3d706f638a5cb4eabb9ea73e7d8a2299e178ec7
Pietro Bersola <pbersola137@gmail.com>
2025-11-12 16:46:03 +0100

    Initial Setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y config-manager --enable fedora-cisco-openh264
+sed -i.bak '/^name=RPM Fusion for Fedora \$releasever - Nonfree$/,/^$/ {
+    /^gpgkey=file:\/\/\/etc\/pki\/rpm-gpg\/RPM-GPG-KEY-rpmfusion-nonfree-fedora-\$releasever$/a\
+exclude=akmod-nvidia* kmod-nvidia* xorg-x11-drv-nvidia* nvidia-*
+}' /etc/yum.repos.d/rpmfusion-nonfree.repo
+dnf5 -y remove firefox firefox-langpacks
+dnf5 -y config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf5 -y install intel-media-driver brave-browser rpmdevtools akmods
+grubby --update-kernel=ALL --args="i915.enable_guc=2 i915.enable_fbc=1"
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
